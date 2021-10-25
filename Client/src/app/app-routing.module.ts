3d7f514555e3dfb045c788dd13050f65b98c3fa3
Aadesh Patil <Aadi2412@hotmail.com>
2021-10-25 23:55:57 +0530

    login
    added login and signup page

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterationComponent } from './pages/registeration/registeration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'index', component: HomeComponent },
+  { path: 'register', component: RegisterationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
